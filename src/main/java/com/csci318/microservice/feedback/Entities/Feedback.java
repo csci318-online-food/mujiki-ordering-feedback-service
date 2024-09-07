5babca1613da2e89501248b0f5a6033b38ca52bf
Elvanss <duy.elvans12@gmail.com>
2024-09-07 14:36:03 +1000

    initalize entity

--- src/main/java/com/csci318/microservice/feedback/Entities/Feedback.java
+++ src/main/java/com/csci318/microservice/feedback/Entities/Feedback.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -15,8 +16,34 @@ import java.util.UUID;
 @Entity
 @Table(name = "feedbacks")
 public class Feedback {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "restaurant_id")
+    private UUID restaurantId;
+
+    @Column(name = "item_id", nullable = true)
+    private UUID itemId;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @Column(name = "comments")
+    private String comments;
+
+    @Column(name = "create_at")
+    private Timestamp createAt;
+
+    @Column(name = "modify_at")
+    private Timestamp modifyAt;
+
+    @Column(name = "modify_by")
+    private String modifyBy;
+
+    @Column(name = "create_by")
+    private String createBy;
 }
--- src/main/resources/logs/db.create.sql
+++ src/main/resources/logs/db.create.sql
@@ -1,3 +1,12 @@
 CREATE TABLE IF NOT EXISTS feedbacks (
- id UUID PRIMARY KEY
+    id UUID PRIMARY KEY,
+    user_id UUID,
+    restaurant_id UUID,
+    item_id UUID NULL,
+    rating INT,
+    comments TEXT,
+    create_at TIMESTAMP,
+    modify_at TIMESTAMP,
+    modify_by VARCHAR(255),
+    create_by VARCHAR(255)
 );
