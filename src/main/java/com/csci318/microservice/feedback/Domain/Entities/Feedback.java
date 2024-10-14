5988323246c9c22e50dcab99ad41a70e1a237ae1
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2024-10-14 23:37:56 +1100

    refactor: Annotations for Feedback service

--- src/main/java/com/csci318/microservice/feedback/Domain/Entities/Feedback.java
+++ src/main/java/com/csci318/microservice/feedback/Domain/Entities/Feedback.java
@@ -1,11 +1,21 @@
 package com.csci318.microservice.feedback.Domain.Entities;
 
-import jakarta.persistence.*;
+import com.csci318.microservice.feedback.Domain.Relations.Restaurant;
+import com.csci318.microservice.feedback.Domain.Relations.User;
+import com.csci318.microservice.feedback.Utils.Annotations.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.AbstractAggregateRoot;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -18,15 +28,16 @@ import java.util.UUID;
 @Table(name = "feedbacks")
 public class Feedback extends AbstractAggregateRoot<Feedback> {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "user_id")
+    @ManyToOne(targetEntity = User.class)
     private UUID userId;
 
     @Column(name = "restaurant_id")
+    @ManyToOne(targetEntity = Restaurant.class)
     private UUID restaurantId;
 
     @Column(name = "rating")
@@ -46,6 +57,4 @@ public class Feedback extends AbstractAggregateRoot<Feedback> {
 
     @Column(name = "create_by")
     private String createBy;
-
-
 }
